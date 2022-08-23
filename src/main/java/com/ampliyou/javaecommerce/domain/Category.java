0d4090330cf9629e2f916a54e3fde55497c7bbb4
JhonnyBaltros <jhonnybaixo@gmail.com>
2022-08-22 22:42:39 -0300

    Adding H2 dev database

--- src/main/java/com/ampliyou/javaecommerce/domain/Category.java
+++ src/main/java/com/ampliyou/javaecommerce/domain/Category.java
@@ -2,15 +2,19 @@ package com.ampliyou.javaecommerce.domain;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
-
-
-@RestController
-@RequestMapping(value = "/categories")
+@Entity
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
 
     public Category() {
